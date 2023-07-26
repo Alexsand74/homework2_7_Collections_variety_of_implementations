378e960e6de6e978c002b22277fc5a9fe190aac8
Alexsand74 <1974alexsand@gmail.com>
2023-07-26 21:28:05 +0300

    homework 2.7. Collections: variety of implementations sheets and sets, the completed task lies here!

--- src/main/java/my/sky/pro/homework2_7/service/EmployeeService.java
+++ src/main/java/my/sky/pro/homework2_7/service/EmployeeService.java
@@ -7,97 +7,50 @@ import my.sky.pro.homework2_7.model.Employee;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class EmployeeService {
     private static final int LIMIT = 10;
-    // в комментариях методы сделаны для массива
-//    private final Employee[] employees;
-    private final List<Employee> employees;
-
-    //    public EmployeeService() {
-//        this.employees = new  Employee[LIMIT];
-//    }
-    public EmployeeService() {
-        this.employees = new ArrayList<>();
-    }
-
-    //    public Employee addEmployee (String firstName, String lastName){
-//        Employee employee = new Employee(firstName,lastName);
-//        int indexForAdd = -1;
-//        for (int i = 0; i < employees.length; i++) {
-//            if(employees[i] == null){
-//                indexForAdd = i;
-//                break;
-//            }
-//            if (employees[i].equals(employee)){
-//                throw new EmployeeAlreadyAddedException();
-//            }
-//        }
-//        if (indexForAdd != -1){
-//            return employees[indexForAdd] = employee;
-//        } else {
-//            throw new EmployeeStorageIsFullException();
-//        }
+    private final Map<String,Employee> employees = new HashMap<>();
+//    private final List<Employee> employees;
+//    public EmployeeService() {
+//        this.employees = new ArrayList<>();
 //    }
     public Employee addEmployee(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employees.contains(employee)) {
+        String key = getKey(firstName,lastName);
+        if (employees.containsKey(key)) {
             throw new EmployeeAlreadyAddedException();
         }
         if (employees.size() < LIMIT) {
-            employees.add(employee);
+            employees.put(key,employee);
             return employee;
         }
         throw new EmployeeStorageIsFullException();
     }
 
     public Employee findEmployee(String firstName, String lastName) {
-        Employee employee = new Employee(firstName, lastName);
-        if (!employees.contains(employee)) {
+        String key = getKey(firstName,lastName);
+        if (!employees.containsKey(key)) {
             throw new EmployeeNotFoundException();
         }
-        return employee;
+        return employees.get(key);
     }
-//    public Employee findEmployee (String firstName, String lastName){
-//        Employee employee = new Employee(firstName,lastName);
-//        for (Employee value :employees) {
-//            if (value.equals(employee)){
-//                return employee;
-//            }
-//        }
-//        throw new EmployeeNotFoundException();
-//    }
-//
-//   public Employee removeEmployee  (String firstName, String lastName) {
-//    Employee employee = new Employee(firstName, lastName);
-//    if (!employees.contains(employee)) {
-//        throw new EmployeeNotFoundException();
-//    }
-//    employees.remove(employee);
-//    return employee;
-//}
     public Employee removeEmployee(String firstName, String lastName) {
-        Employee employee = new Employee(firstName, lastName);
-        if (!employees.remove(employee)) {
-            throw new EmployeeNotFoundException();
+        String key = getKey(firstName,lastName);
+        if (!employees.containsKey(key)) {
+                  throw new EmployeeNotFoundException();
         }
-        return employee;
+        return employees.remove(key);
     }
 
-//    public Employee removeEmployee (String firstName, String lastName){
-//        Employee employee = new Employee(firstName,lastName);
-//        for (int i = 0; i < employees.length; i++) {
-//            if (employees[i].equals(employee)){
-//                employees[i] = null;
-//                return employee;
-//            }
-//        }
-//        throw new EmployeeNotFoundException();
-//    }
     public List<Employee> getAll() {
-//        return Collections.unmodifiableList(employees) // делает list не модифицируемым
-        return new ArrayList<>(employees);
+        return new ArrayList<>(employees.values());
+    }
+    public String getKey (String firstName, String lastName){
+        return firstName+lastName;
     }
 }
